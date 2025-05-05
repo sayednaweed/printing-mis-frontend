b8fad8e8dd9a92afcc393ef8224a92d8200a3401
Your Name <you@example.com>
2025-05-05 15:24:13 +0430

    Working on add leave page

--- src/views/pages/auth/hr/leave/leave-page.tsx
+++ src/views/pages/auth/hr/leave/leave-page.tsx
@@ -1,5 +1,28 @@
+import { useNavigate } from "react-router";
+import { useTranslation } from "react-i18next";
+import { LeaveTable } from "./leave-table";
+import UserHeader from "../users/user-header";
+import {
+  Breadcrumb,
+  BreadcrumbHome,
+  BreadcrumbItem,
+  BreadcrumbSeparator,
+} from "@/components/custom-ui/Breadcrumb/Breadcrumb";
 export default function LeavePage() {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+  const handleGoBack = () => navigate(-1);
+  const handleGoHome = () => navigate("/dashboard", { replace: true });
+
   return (
-    <div className=" h-screen flex items-center justify-center">LeavePage</div>
+    <div className="px-2 pt-2 flex flex-col gap-y-[2px] relative select-none rtl:text-2xl-rtl ltr:text-xl-ltr">
+      <Breadcrumb>
+        <BreadcrumbHome onClick={handleGoHome} />
+        <BreadcrumbSeparator />
+        <BreadcrumbItem onClick={handleGoBack}>{t("users")}</BreadcrumbItem>
+      </Breadcrumb>
+      <UserHeader />
+      <LeaveTable />
+    </div>
   );
 }
